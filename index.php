9959609b6bae7f2b1f6373104c9e29126d2faeb1
naufalbintang <naufalbintangpradana@gmail.com>
2024-07-24 13:39:46 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cumque, explicabo velit iste reiciendis ducimus vitae consectetur alias recusandae voluptatibus totam exercitationem dicta. Corrupti commodi tempora at, aliquam voluptatibus praesentium!</p>
 </body>
 </html>
