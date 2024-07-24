4c5a527bf4334d91967751e7f15f283eb9aba783
naufalbintang <naufalbintangpradana@gmail.com>
2024-07-24 13:43:32 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cumque, explicabo velit iste reiciendis ducimus vitae consectetur alias recusandae voluptatibus totam exercitationem dicta. Corrupti commodi tempora at, aliquam voluptatibus praesentium!</p>
+
+    <li>satu</li>
+    <li>dua</li>
 </body>
 </html>
